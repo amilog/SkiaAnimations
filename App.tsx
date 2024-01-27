646d88ddb44bd3b73f99395e759fc04e02cd7034
amilgasimov <amillgasimov@gmail.com>
2024-01-27 23:29:24 +0400

    test

--- App.tsx
+++ App.tsx
@@ -1,14 +1,27 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import {ChasingTouchBubbles} from './src/Animations/ChasingTouchBubbles';
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
+    <View style={styles.container}>
+      <ChasingTouchBubbles />
+      <Text style={styles.text}>@CodeByAmil</Text>
     </View>
   );
 };
 
 export default App;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  text: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    color: 'white',
+    fontSize: 20,
+  },
+});

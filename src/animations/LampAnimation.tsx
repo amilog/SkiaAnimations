29d40261e0e0bc4763071dff2b58766b641b5db1
Amil Gasimov <93605485+amilog@users.noreply.github.com>
2024-04-23 15:51:26 +0400

    add animation

--- src/animations/LampAnimation.tsx
+++ src/animations/LampAnimation.tsx
@@ -1,21 +1,59 @@
-import React from 'react';
-import {Pressable, StyleSheet, View} from 'react-native';
+import React, {useState} from 'react';
+import {Animated, Pressable, StyleSheet, View} from 'react-native';
 import Metrics from '../constants/metrics';
 import LinearGradient from 'react-native-linear-gradient';
 
 function LampAnimation() {
+  const [lampOn, setLampOn] = useState(false);
+  const [lampColor] = useState(new Animated.Value(0));
+
   const handlePress = () => {
     console.log('Pressed');
+    setLampOn(!lampOn);
+
+    Animated.timing(lampColor, {
+      toValue: lampOn ? 0 : 1,
+      duration: 400,
+      useNativeDriver: false,
+    }).start();
   };
+
+  const interpolatedColor = lampColor.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['#A2A2A2', 'rgba(255,255,0,1)'],
+  });
+
   return (
     <View style={styles.view}>
+      <View style={styles.whiteContainer} />
       <View style={styles.lampRectangular} />
-      <View style={styles.lamp} />
-      <View style={styles.leftContainer} />
-      <LinearGradient
-        colors={['rgba(255,255,0,0.4)', 'rgba(255,255,0,0.1)']}
-        style={styles.light}
+      <Animated.View
+        style={[styles.lamp, {backgroundColor: interpolatedColor}]}
       />
+      <View style={styles.leftContainer} />
+      <Animated.View
+        style={[
+          styles.light,
+          {
+            opacity: lampColor,
+            transform: [
+              {
+                translateY: Animated.multiply(
+                  lampColor.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [Metrics.rem * 580, 0],
+                  }),
+                  -1,
+                ),
+              },
+            ],
+          },
+        ]}>
+        <LinearGradient
+          colors={['rgba(255,255,0,0.5)', 'rgba(255,255,0,0)']}
+          style={styles.gradient}
+        />
+      </Animated.View>
       <View style={styles.rightContainer} />
       <View style={styles.trapezoid} />
       <View style={styles.lampRope} />
@@ -55,7 +93,6 @@ const styles = StyleSheet.create({
     width: Metrics.rem * 100,
     height: Metrics.rem * 100,
     borderRadius: Metrics.rem * 50,
-    backgroundColor: '#A2A2A2', //'rgba(255,255,0,1)'
     position: 'absolute',
     top: Metrics.rem * 290,
   },
@@ -85,6 +122,12 @@ const styles = StyleSheet.create({
     zIndex: 1,
     transform: [{skewY: '-70deg'}],
   },
+  whiteContainer: {
+    width: Metrics.rem * 500,
+    height: Metrics.rem * 320,
+    backgroundColor: 'white',
+    zIndex: 1,
+  },
   lampRope: {
     width: Metrics.rem * 2,
     height: Metrics.rem * 150, // 210
@@ -103,5 +146,11 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: Metrics.rem * 445, // 480
     right: Metrics.rem * 136.5,
+  },
+  gradient: {
+    width: Metrics.WIDTH,
+    height: Metrics.rem * 580,
+    position: 'absolute',
+    top: 0,
   },
 });
